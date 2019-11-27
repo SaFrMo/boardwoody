f28601bd20f3ad081990b55af820d50eb3b208ac
Sander Moolin <sandermoolin@gmail.com>
2019-11-27 16:01:01 -0500

    WIP on block library

---
+++ components/GameWrap.vue
@@ -0,0 +1,14 @@
+<template>
+    <client-only>
+        <section class="game-wrap" v-if="G">{{ G.grid }}</section>
+    </client-only>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.game-wrap {
+}
+</style>
